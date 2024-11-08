492d58be55e8f3c285fb217c3c33fcaf920cebab
sean <sean.g.crim@gmail.com>
2024-11-08 12:45:17 -0500

    more tutorial followed almost ready to start refactor

--- flask_mega_tutorial/app/models.py
+++ flask_mega_tutorial/app/models.py
@@ -2,18 +2,98 @@ from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+#association table
+#keeping track of the many to many following relationships
+#one user can follow many people
+#one user can be followed by many people
+#allows query of scenario of All Followers Where Followed_ID = user.id
+#allows query of scenario of ALL Followed Where Follower_ID = user.id
+followers = sa.Table(
+    'followers',
+    db.metadata,
+    sa.Column('follower_id', sa.Integer, sa.ForeignKey('user.id'), primary_key=True),
+    sa.Column('followed_id', sa.Integer, sa.ForeignKey('user.id'), primary_key=True)
+)
+
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc))
+    following: so.WriteOnlyMapped['User'] = so.relationship(secondary=followers, primaryjoin=(followers.c.follower_id == id), secondaryjoin=(followers.c.followed_id == id), back_populates='followers')
+    followers: so.WriteOnlyMapped['User'] = so.relationship(secondary=followers, primaryjoin=(followers.c.followed_id == id), secondaryjoin=(followers.c.follower_id == id), back_populates='following')
 
     posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+
+    def follow(self, size):
+        if not self.is_following(user):
+            self.following.add(user)
+    
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.following.remove(user)
+
+    def is_following(self, user):
+        query = self.following.select().where(User.id == user.id)
+        return db.session.scalar(query) is not None
+
+    def followers_count(self):
+        query = sa.select(sa.func.count()).select_from(
+            self.followers.select().subquery())
+        return db.session.scalar(query)
+
+    def following_count(self):
+        query = sa.select(sa.func.count()).select_from(
+            self.following.select().subquery())
+        return db.session.scalar(query)
+
+    #this is nuts
+    def following_posts(self):
+        #this is giving an alias for each User object
+        #one user we want to join with is Author
+        #other user we want to join with is Follower
+        Author = so.aliased(User)
+        Follower = so.aliased(User)
+        #--original query structure--
+        #1- select all posts
+        #2- join all posts with their authors
+        #3- join all posts with the people following the authors
+        #4- filter out to just see all posts from authors the current user is following
+        #5- order by
+        #--new query structure, includes posts by current user--
+        # the OR makes it so we get -- all posts by who the curret user is following -- PLUS -- all posts where the author is the current user --
+        return (
+            sa.select(Post)
+            .join(Post.author.of_type(Author))
+            .join(Author.followers.of_type(Follower), isouter=True)
+            .where(sa.or_(Follower.id == self.id, Author.id == self.id))
+            .group_by(Post)
+            .order_by(Post.timestamp.desc())
+        )
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
--- flask_mega_tutorial/app/routes.py
+++ flask_mega_tutorial/app/routes.py
@@ -1,6 +1,39 @@
 from app import app
+from flask_login import current_user, login_user, logout_user, login_required
+from urllib.parse import urlsplit
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     return "HELLO BRO"
+
+@app.route('/login', methods=['POST',])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = db.session.scalar(
+        sa.select(User).where(User.username == 'add the json handling from the body of request here')
+    )
+    if user is None or not user.check_password('add the json handling from the body of the request here'):
+        flash('Invalid username or password')
+        return redirect(url_for('login'))
+    #TODO pull remember me flag from json request body
+    login_user(user, remember=True)
+    next_page = request.args.get('next')
+    if not next_page or urlsplit(next_page).netloc != '':
+        next_page = url_for('index')
+    return redirect(next_page)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/user/<username>')
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    return user
